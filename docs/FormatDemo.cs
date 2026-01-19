2067da9354a39f3f3069726f515c05f323e5b4ac
xc8pt <brolike636@gmail.com>
2026-01-19 13:05:41 +0300

    Added text formatting examples and explanations

---
+++ docs/FormatDemo.cs
@@ -0,0 +1,6 @@
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+int sum = num1 + num2;
+Console.WriteLine($"**Результат операций: {sum}**");
